1936d440d162a5f94c88d19dea54d57360378228
Martijn de Vos <martijn@code-up.nl>
2017-01-09 11:51:47 +0100

    Forgot to add login URL

--- errorreporter/errorreporter/urls.py
+++ errorreporter/errorreporter/urls.py
@@ -5,6 +5,7 @@ from errorreporter import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index$', views.index, name='index'),
+    url(r'^login', views.login, name='login'),
     url(r'^overview_crashreport_daily$', views.overview_crashreport_daily, name='overview_daily'),
     url(r'^overview_crashreport_version$', views.overview_crashreport_version, name='overview_version'),
     url(r'^crashreport_daily/(?P<date>\d{4}-\d{2}-\d{2})$', views.crashreport_daily, name='crashreport_daily'),
