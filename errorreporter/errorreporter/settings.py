163b5ff5246b41d3a4c9e5ba35be32e40e84e99a
Martijn de Vos <martijn@code-up.nl>
2017-01-09 11:25:14 +0100

    Changed db path

--- errorreporter/errorreporter/settings.py
+++ errorreporter/errorreporter/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'errorreporter.urls'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/var/lib/bug_reporter', 'db.sqlite3'),
     }
 }
 
